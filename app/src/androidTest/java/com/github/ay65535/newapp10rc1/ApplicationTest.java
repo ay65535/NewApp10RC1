630beac85531b03b2047532ba7ed4d2524980239
ay65535 <ay65535@icloud.com>
2014-11-21 20:14:38 +0900

    Add ApplicationTestCase

--- app/src/androidTest/java/com/github/ay65535/newapp10rc1/ApplicationTest.java
+++ app/src/androidTest/java/com/github/ay65535/newapp10rc1/ApplicationTest.java
@@ -2,6 +2,8 @@ package com.github.ay65535.newapp10rc1;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +11,23 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @SmallTest
+    public void testPreconditions() {
+        assertEquals(1,1);
+    }
+
+    /**
+     * Test basic startup/shutdown of Application
+     */
+    @MediumTest
+    public void testSimpleCreate() {
+        createApplication();
     }
 }
